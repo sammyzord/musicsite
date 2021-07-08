59e8a666af9472e355588bd76f0ddcdfc4cc8c77
Samuel Borges <borges.sb@protonmail.com>
2021-07-08 00:16:15 -0300

    refactor: hides secret key

--- musicsite/settings.py
+++ musicsite/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fw3f*xp3yzn@3=j@v-db!+g%_^t@)mc^7_r-_y-(26ue*n7_q="
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
 ALLOWED_HOSTS = []
 
