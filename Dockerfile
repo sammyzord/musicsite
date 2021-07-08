5844f2ac1f40349ab7aed487c314b1354582b250
Samuel Borges <borges.sb@protonmail.com>
2021-07-08 00:06:55 -0300

    feature: adds continuous deployment to CI

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "-b", "0.0.0.0:8000", "musicsite.wsgi"]
+CMD python3 manage.py migrate && gunicorn -b  0.0.0.0:$PORT musicsite.wsgi
--- musicsite/settings.py
+++ musicsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 import os
 from pathlib import Path
 
@@ -153,3 +153,5 @@ LOGGING = {
         },
     },
 }
+
+django_heroku.settings(locals())
